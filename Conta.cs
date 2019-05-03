f346654a5b28c696b67fbc9da7c425f3bcf23c20
Gabriel Dias <gabrieldias2032@gmail.com>
2019-05-03 00:21:48 -0300

    Versão 1.0

---
+++ Conta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePad2C_GabrielDias_EduardoFrans
+{
+    class Conta
+    {
+        int numeroConta;
+        string nome;
+        int saldo;
+
+        public string mostrarNome()
+        {
+            return this.nome;
+        }
+        public int mostrarNumeroConta()
+        {
+            return this.numeroConta;
+        }
+        public int mostrarSaldo()
+        {
+            return this.saldo;
+        }
+
+        public Conta()
+        {
+            this.saldo = 0;
+            this.numeroConta = 0;
+            this.nome = "";
+        }
+        public Conta(int numeroConta, string nome, int saldo)
+        {
+            this.saldo = saldo;
+            this.numeroConta = numeroConta;
+            this.nome = nome;
+        }
+        public void debitarValor(int debito)
+        {
+            if(this.saldo - debito < 0)
+            {
+                this.saldo = 0;
+            }
+            else
+            {
+                this.saldo -= debito;
+            }
+        }
+        public void creditarValor(int credito)
+        {
+            this.saldo += credito;
+        }
+        public void transferencia(Conta contaDestino, int valor)
+        {
+            debitarValor(valor);
+            contaDestino.creditarValor(valor);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePad2C_GabrielDias_EduardoFrans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração dos objetos e variaveis
+            Conta cliente1 = new Conta(1234, "Maria Fernanda", 0);
+            Conta cliente2 = new Conta(1235, "Paulo Ricardo", 0);
+            string C = "Nº da conta: ";
+            string N = "\nNome: ";
+            string S = "\nSaldo: ";
+            //exercicio
+            cliente2.creditarValor(100);
+            cliente1.creditarValor(250);
+            cliente1.debitarValor(35);
+            cliente1.transferencia(cliente2, 20);
+            Console.WriteLine(C + cliente1.mostrarNumeroConta() + N + cliente1.mostrarNome() + S + cliente1.mostrarSaldo());
+            Console.WriteLine("\n" + C + cliente2.mostrarNumeroConta() + N + cliente2.mostrarNome()+ S + cliente2.mostrarSaldo());
+
+
+
+        }
+    }
+}
